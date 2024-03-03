2e4fb3f6a1f4ef87f319023bd0c576057948e6fa
Martijn Sassen <sassenjm@gmail.com>
2024-03-03 15:13:42 +0100

    replaced single with double quotes

--- csv2ics.py
+++ csv2ics.py
@@ -22,27 +22,27 @@ cols = {
 totalEvents = 0
 fCounter = 0  # Counter for ics files created
 now = datetime.now(timezone.utc)
-nowical = now.strftime('%Y%m%dT%H%M%SZ')
+nowical = now.strftime("%Y%m%dT%H%M%SZ")
 
 # open and read file
-print('Opening Calendar.csv...')
+print("Opening Calendar.csv...")
 try:
-    csvFile = open('Calendar.csv', encoding='utf-8-sig')
+    csvFile = open("Calendar.csv", encoding="utf-8-sig")
     data = csv.DictReader(csvFile)
 except:
     print("Error while opening Calendar.csv")
     print("Check the file is present and has the right name\n")
-    os.system('pause')
+    os.system("pause")
     exit()
 
 print("Calendar.csv opened")
 
 if not all (k in data.fieldnames for k in cols.values()):
     print("Error: Required columns missing in Calendar.csv")
-    print("You have: " + ', '.join(data.fieldnames))
-    print("You need: " + ', '.join(cols.values()))
+    print("You have: " + ", ".join(data.fieldnames))
+    print("You need: " + ", ".join(cols.values()))
     print("The order is not important\n")
-    os.system('pause')
+    os.system("pause")
     exit()
 
 # sort events (create calendars)
@@ -56,59 +56,59 @@ for row in data:
     except:
         csvFile.close()
         print("Error while sorting events\n")
-        os.system('pause')
+        os.system("pause")
         exit()
 print("Events sorted")
 
 print("Creating ics files...")
 # create ics files
 for key, events in c.items():
-    f = open('ics/'+key+'.ics', "w", encoding='utf-8')
+    f = open("ics/"+key+".ics", "w", encoding="utf-8")
     # file header
-    f.write('BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//MSASSEN//CSV2ICS//EXPORT\nCALSCALE:GREGORIAN\n')
+    f.write("BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//MSASSEN//CSV2ICS//EXPORT\nCALSCALE:GREGORIAN\n")
     eCounter = 0
 
     for e in events:
         # event header
-        f.write('BEGIN:VEVENT\n')
-        f.write('DTSTAMP:' + nowical + '\n')
-        f.write(f'UID:CdL-{nowical}F{fCounter}E{eCounter}\n')
+        f.write("BEGIN:VEVENT\n")
+        f.write("DTSTAMP:" + nowical + "\n")
+        f.write(f"UID:CdL-{nowical}F{fCounter}E{eCounter}\n")
 
         # event data
         if e[cols["day"]] == "TRUE":
-            start = datetime.strptime(e[cols["sda"]], '%d.%m.%Y')
-            end = datetime.strptime(e[cols["eda"]], '%d.%m.%Y') + timedelta(days=1)
-            f.write('DTSTART;VALUE=DATE:' + start.strftime('%Y%m%d') + '\n')
-            f.write('DTEND;VALUE=DATE:' + end.strftime('%Y%m%d') + '\n')
+            start = datetime.strptime(e[cols["sda"]], "%d.%m.%Y")
+            end = datetime.strptime(e[cols["eda"]], "%d.%m.%Y") + timedelta(days=1)
+            f.write("DTSTART;VALUE=DATE:" + start.strftime("%Y%m%d") + "\n")
+            f.write("DTEND;VALUE=DATE:" + end.strftime("%Y%m%d") + "\n")
         else:
-            start = datetime.strptime(e[cols["sda"]]+' '+e[cols["sti"]], '%d.%m.%Y %H:%M')
-            end = datetime.strptime(e[cols["eda"]]+' '+e[cols["eti"]], '%d.%m.%Y %H:%M')
-            f.write('DTSTART:' + start.strftime('%Y%m%dT%H%M%S') + '\n')
-            f.write('DTEND:' + end.strftime('%Y%m%dT%H%M%S') + '\n')
+            start = datetime.strptime(e[cols["sda"]]+" "+e[cols["sti"]], "%d.%m.%Y %H:%M")
+            end = datetime.strptime(e[cols["eda"]]+" "+e[cols["eti"]], "%d.%m.%Y %H:%M")
+            f.write("DTSTART:" + start.strftime("%Y%m%dT%H%M%S") + "\n")
+            f.write("DTEND:" + end.strftime("%Y%m%dT%H%M%S") + "\n")
 
-        f.write('SUMMARY:' + e[cols["sub"]] + '\n')
-        f.write('LOCATION:' + e[cols["loc"]] + '\n')
-        f.write('STATUS:CONFIRMED' + '\n')
-        f.write('DESCRIPTION:' + e[cols["dsc"]] + '\n')
+        f.write("SUMMARY:" + e[cols["sub"]] + "\n")
+        f.write("LOCATION:" + e[cols["loc"]] + "\n")
+        f.write("STATUS:CONFIRMED" + "\n")
+        f.write("DESCRIPTION:" + e[cols["dsc"]] + "\n")
         
         # event footer
-        f.write('END:VEVENT\n')
+        f.write("END:VEVENT\n")
         eCounter += 1
 
     # file footer
-    f.write('END:VCALENDAR')
+    f.write("END:VCALENDAR")
     f.close()
 
     # end of file
     totalEvents += eCounter
     fCounter += 1
-    print(f'{key}.ics created: {eCounter} event(s)')
+    print(f"{key}.ics created: {eCounter} event(s)")
 
-print('Closing Calendar.csv...')
+print("Closing Calendar.csv...")
 csvFile.close()
 print("Calendar.csv closed")
 
-print('ICS files created successfully')
-print(f'{fCounter} files created for a total of {totalEvents} event(s)')
+print("ICS files created successfully")
+print(f"{fCounter} files created for a total of {totalEvents} event(s)")
 print()
-os.system('pause')
+os.system("pause")
